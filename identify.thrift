e32c7ba345a9b586c9f4506d70d37c287dc1296b
root <root@xc.(none)>
2017-12-22 09:27:53 +0800

    增加 identify.thrif

---
+++ identify.thrift
@@ -0,0 +1,13 @@
+service  FaceIdentify {
+ void FI_add_face_database(1: string uid, 2: string user_info, 3: string group_id, 4: string imagename),
+ i32 FI_del_face_database(1: string uid, 2: string group_id),
+ i32 FI_update_face_database(1: string uid, 2: string user_info, 3: string group_id, 4: string imagename),
+ i32 FI_find_user_info(1: string uid),
+ string FI_find_group_users(1: string group_id),
+ string FI_face_database_identify(1: string group_id, 2: string imagename),
+ string FI_face_database_verify(1: string uid, 2: string user_info, 3: string group_id, 4: string imagename),
+ i32 FI_group_deleteuser(1: string uid, 2: string group_id),
+ i32 FI_group_adduser(1:string src_group_id, 2: string group_id, 3: string uid)
+}
+
+
