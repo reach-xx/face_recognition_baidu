0cd12025123b6ba56bf734e37e85c35c82c7d0de
root <root@xc.(none)>
2017-12-28 10:25:39 +0800

    备份identify.thrift文件

--- identify.thrift
+++ identify.thrift
@@ -8,6 +8,8 @@ service  FaceIdentify {
  string FI_face_database_verify(1: string uid, 2: string user_info, 3: string group_id, 4: string imagename),
  i32 FI_group_deleteuser(1: string uid, 2: string group_id),
  i32 FI_group_adduser(1:string src_group_id, 2: string group_id, 3: string uid)
+ string FI_face_detect(1:string imagename)
+ string FI_face_match(1:string image1, 2:string image2)  
 }
 
 
